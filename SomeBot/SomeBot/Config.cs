37ae73ffc50d24cab37fe985d42d3ecfd1f73149
Nikita Yurishev <chelovek438@gmail.com>
2021-03-22 12:34:50 +0300

    Add files via upload
    hjnjjjjjjjjjjjjjjjjjjjjh

---
+++ SomeBot/SomeBot/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SomeBot
+{
+    public class Config
+    { 
+        public static string apiToken = "9ae6e2017fe89ec81982e0011eaf31a6";
+        public static string telegramToken = "1443507962:AAGEinwDiH8pk4vlapa67J6UsUsjslH_b4Y";
+    }
+}
